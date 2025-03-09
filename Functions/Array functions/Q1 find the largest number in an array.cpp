7dd244e5842faf1ccbdc78a2b42cbfa709780cb5
harsh14thakre <harsh14thakre@gmail.com>
2025-03-09 23:24:04 +0530

    updated

--- Functions/Array functions/Q1 find the largest number in an array.cpp
+++ Functions/Array functions/Q1 find the largest number in an array.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 
-int largest()
+int largest(int arr[])
 {
     int large=0;
     for(int i=0; i<9; ++i)
@@ -11,12 +11,14 @@ int largest()
             large==arr[i];
         }
     }
-    cout<<"The largest value in this array is"<<large;
+    return large;
+    
 }
 
 int main()
 {
 
     int arr[]={2,1,3,4,5,6,9,8,7};
-    largest();
+    largest(arr[]);
+    cout<<"The largest value in this array is"<<large;
 }
